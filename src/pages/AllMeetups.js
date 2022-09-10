4005d9ebe74d6bff3f9fa6531a77884685d1795c
debkidder <wuttakidder@gmail.com>
2022-09-10 10:36:25 -0600

    reverting to before CORS error to save off an  error-free branch

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,3 @@
-import { useState, useEffect } from "react";
-// import { response } from "express";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_DATA = [
@@ -24,55 +22,10 @@ const DUMMY_DATA = [
 ];
 
 function AllMeetupsPage() {
-  // component function
-  const [isLoading, setIsLoading] = useState(true);
-  // useState will always return array of these two elements:
-  // isLoading is current state snapshot; setIsLoading is function for updating the state
-  const [loadedMeetups, setLoadedMeetups] = useState([]);
-
-  useEffect(() => {
-    // side effects that shouldn't run in some situations
-    setIsLoading(true);
-    fetch(
-      "https://react-meetup-firebase-default-rtdb.firebaseio.com/meetups/-NBZvtVLQxvCcSXYasuZ"
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        // need to transform data that it's fetching (stored as object, but 'map' needs an array)
-        const meetups = [];
-
-        for (const key in data) {
-          const meetup = {
-            id: key,
-            ...data[key],
-          };
-
-          // helper meetups array
-          meetups.push(meetup);
-        }
-
-        setIsLoading(false);
-        setLoadedMeetups(data);
-        // will cause infinite loop if we forget to use the "useEffect" hook
-      });
-  }, []);
-  // checks the dependencies array to see if it should execute the function (ours is empty because there are no external dependencies)
-
-  if (isLoading) {
-    return (
-      <section>
-        <p>Loading...</p>
-      </section>
-    );
-  }
-
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={loadedMeetups} />
-      {/* <MeetupList meetups={DUMMY_DATA} /> */}
+      <MeetupList meetups={DUMMY_DATA} />
     </section>
   );
 }
